b9100b7aa72943e99600d49445268ad2d6fa9f0e
adogbeji <baka_1993@yahoo.co.uk>
2025-03-08 09:38:38 +0000

    Start defining colours in AppColors class

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -1,1 +1,7 @@
-class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const primary = Color(0xFF42C83C);
+  static const lightBackground = Color(0xFFF2F2F2);
+}
