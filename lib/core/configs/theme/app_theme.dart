ab5d49d5493edddad87f88fdd559bf0b008611e2
adogbeji <baka_1993@yahoo.co.uk>
2025-04-03 14:48:34 +0100

    Start defining app light theme

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -1,1 +1,13 @@
-class AppTheme {}
+import 'package:flutter/material.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(),
+    ),
+  );
+}
